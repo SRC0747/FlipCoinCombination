8a2a004ae770cb85916770993a8d117bef7403c9
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 13:48:18 +0530

    Storing Singlet combination in a dictionary and calculating Singlet Percentage.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,9 +2,22 @@
 echo "Welcome to Flip Coin Combination problem to calculate winning percentage of Head or Tail in Singlet , Doublet and Triptet."
 isCheck=1
 randomCheck=$((RANDOM%2))
-if [ $isCheck -eq $randomCheck ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+head_value=0
+tail_value=0
+count=1
+while [ $count -gt 0 ]
+do
+	for (( i=1; i<=5; i++ ))
+	do
+		if [ $isCheck -eq $randomCheck ]
+		then
+			head_value=$(($head_value+1))
+			dice[$(($i+1))]=$head_value
+		else
+			tail_value=$(($tail_value+1))
+			dice[$(($i+1))]=$head_value
+		fi
+		((count++))
+	done
+done
+singlet_percent=$(((1/6)*100))
