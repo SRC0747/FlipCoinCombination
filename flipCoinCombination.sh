bcf6a8fb33c46c1344a8ab35d55107a4ed4f355c
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 14:19:13 +0530

    Storing Doublet combination in a dictionary and percentage calculation.

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,6 +5,9 @@ randomCheck=$((RANDOM%2))
 head_value=0
 tail_value=0
 count=1
+declare -a times
+declare -A dice_singlet
+declare -A dice_doublet
 while [ $count -gt 0 ]
 do
 	for (( i=1; i<=5; i++ ))
@@ -12,12 +15,27 @@ do
 		if [ $isCheck -eq $randomCheck ]
 		then
 			head_value=$(($head_value+1))
-			dice[$(($i+1))]=$head_value
+			dice_singlet[$(($i+1))]=$head_value
 		else
 			tail_value=$(($tail_value+1))
-			dice[$(($i+1))]=$head_value
+			dice_singlet[$(($i+1))]=$head_value
 		fi
 		((count++))
 	done
 done
+echo "${dice_singlet[@]}"
 singlet_percent=$(((1/6)*100))
+for(i=0;i<360000;i++)
+do
+		a=$(((RANDOM%6)+1))
+		b=$(((RANDOM%6)+1))
+		sum=$(($a+$b))
+		times[$sum-2]=$((times[$sum-2]+1))
+done
+
+for (( i=1; i<=11; i++ ))
+do
+	dice_doublet[$(($i+1))]=$(times[$i])
+done
+echo "${dice_doublet[@]}"
+doublet_percent=$(((1/36)*100))
