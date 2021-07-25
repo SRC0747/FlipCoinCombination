3dfc841e1a961e4e4d3497b2d47484f51acfd36e
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 13:17:29 +0530

    Checking Head or Tail by using ((RANDOM))

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Combination problem to calculate winning percentage of Head or Tail in Singlet , Doublet and Triptet."
-
+isCheck=1
+randomCheck=$((RANDOM%2))
+if [ $isCheck -eq $randomCheck ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
